e6e3894c537e2ab986ad0effb76efc2e56c38334
lupalianoferre <lpaliano2013@gmail.com>
2025-10-05 09:11:20 -0300

    Favicon y Dark Card
    Cambiar diseño de las tarjetas a modo oscuro y agregar logo favicon a los heads.

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded", () => {
 //añadir footer
 document.addEventListener("DOMContentLoaded", () => {
   // Cargar navbar
-fetch('footer.html')
+  fetch('footer.html')
     .then(response => response.ok ? response.text() : Promise.reject('Error al cargar footer.html'))
     .then(html => {
       const footerDiv = document.createElement('div');
@@ -28,5 +28,16 @@ fetch('footer.html')
     .catch(error => {
       console.error('Error cargando footer:', error);
     });
+});
+
+
+// Cambia la imagen del modal al abrir en menu.html
+var imageModal = document.getElementById('imageModal')
+imageModal.addEventListener('show.bs.modal', function (event) {
+  var img = event.relatedTarget; // Imagen que disparó el modal
+  var src = img.getAttribute('data-bs-image');
+  var modalImg = imageModal.querySelector('#modalImage');
+  modalImg.src = src;
+  modalImg.alt = img.alt;
 });
 
